39d52196afdf9f30725fa4f6a1ba340ac4407aab
Martin Linkhorst <m.linkhorst@googlemail.com>
2011-03-09 23:33:20 +0100

    log4cxx

---
+++ log4cxx/main.cpp
@@ -0,0 +1,30 @@
+// http://www.yolinux.com/TUTORIALS/Log4cxx.html
+
+#include <log4cxx/logger.h>
+#include <log4cxx/xml/domconfigurator.h>
+
+// Define static logger variable
+log4cxx::LoggerPtr loggerMyMain(log4cxx::Logger::getLogger("main"));
+log4cxx::LoggerPtr loggerFunctionA(log4cxx::Logger::getLogger("functionA"));
+
+void functionA()
+{
+   LOG4CXX_INFO(loggerFunctionA, "Executing functionA.");
+}
+
+int main(int argc, const char **argv)
+{
+   // Load configuration file
+   log4cxx::xml::DOMConfigurator::configure("Log4cxxConfig.xml");
+
+   LOG4CXX_TRACE(loggerMyMain, "this is a debug message for detailed code discovery.");
+   LOG4CXX_DEBUG(loggerMyMain, "this is a debug message.");
+   LOG4CXX_INFO(loggerMyMain, "this is a info message, ignore.");
+   LOG4CXX_WARN(loggerMyMain, "this is a warn message, not too bad.");
+   LOG4CXX_ERROR(loggerMyMain, "this is a error message, something serious is happening.");
+   LOG4CXX_FATAL(loggerMyMain, "this is a fatal message!!!");
+
+   functionA();
+
+   return EXIT_SUCCESS;
+}
