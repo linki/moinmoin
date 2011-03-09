554c507e57faa8febcf28deb61c32574575609f9
Martin Linkhorst <m.linkhorst@googlemail.com>
2011-03-10 00:07:04 +0100

    go

---
+++ go/main.go
@@ -0,0 +1,9 @@
+// http://golang.org/doc/go_tutorial.html
+
+package main
+
+import fmt "fmt"  // Package implementing formatted I/O.
+
+func main() {
+	fmt.Printf("Hello, world; or Καλημέρα κόσμε; or こんにちは 世界\n")
+}
