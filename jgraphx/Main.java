51cfe1a1f834a6066b6c82d88bebc04f6409cf61
Martin Linkhorst <m.linkhorst@googlemail.com>
2011-03-11 19:32:33 +0100

    jgraphx: intendation fix

--- jgraphx/Main.java
+++ jgraphx/Main.java
@@ -9,29 +9,29 @@ import com.mxgraph.view.mxGraph;
 public class Main extends JFrame {
     private static final long serialVersionUID = -2707712944901661771L;
 
-	public Main() {
-		super("Hello, World!");
+    public Main() {
+        super("Hello, World!");
 
-		mxGraph graph = new mxGraph();
-		Object parent = graph.getDefaultParent();
+        mxGraph graph = new mxGraph();
+        Object parent = graph.getDefaultParent();
 
-		graph.getModel().beginUpdate();
-		try {
-			Object v1 = graph.insertVertex(parent, null, "Hello", 20, 20, 80, 30);
-			Object v2 = graph.insertVertex(parent, null, "World!", 240, 150, 80, 30);
-			graph.insertEdge(parent, null, "Edge", v1, v2);
-		} finally {
-			graph.getModel().endUpdate();
-		}
+        graph.getModel().beginUpdate();
+        try {
+            Object v1 = graph.insertVertex(parent, null, "Hello", 20, 20, 80, 30);
+            Object v2 = graph.insertVertex(parent, null, "World!", 240, 150, 80, 30);
+            graph.insertEdge(parent, null, "Edge", v1, v2);
+        } finally {
+            graph.getModel().endUpdate();
+        }
 
-		mxGraphComponent graphComponent = new mxGraphComponent(graph);
-		getContentPane().add(graphComponent);
-	}
+        mxGraphComponent graphComponent = new mxGraphComponent(graph);
+        getContentPane().add(graphComponent);
+    }
 
-	public static void main(String[] args) {
-		Main frame = new Main();
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.setSize(400, 320);
-		frame.setVisible(true);
-	}
+    public static void main(String[] args) {
+        Main frame = new Main();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(400, 320);
+        frame.setVisible(true);
+    }
 }
